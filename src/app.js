59c308d0cd5fac411f51c87dc63bb4e77bd5d4e0
klbailey <135627670+klbailey@users.noreply.github.com>
2023-11-20 16:13:07 +0000

    Delete img in template

--- src/app.js
+++ src/app.js
@@ -9,3 +9,7 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
