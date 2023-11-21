98e9437842d744c4e1b94c62c0fa980765e5d7f3
klbailey <135627670+klbailey@users.noreply.github.com>
2023-11-21 22:03:13 +0000

    Cleaned code

--- src/app.js
+++ src/app.js
@@ -11,32 +11,15 @@ window.onload = function() {
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
 
+  //Loop through each array in nested loop
   for (let pronounIndex = 0; pronounIndex < pronoun.length; pronounIndex++) {
     for (let adjIndex = 0; adjIndex < adj.length; adjIndex++) {
       for (let nounIndex = 0; nounIndex < noun.length; nounIndex++) {
         console.log(
+          //Concat one per array and add .com at end
           pronoun[pronounIndex] + adj[adjIndex] + noun[nounIndex] + ".com"
         );
       }
     }
   }
 };
-
-// empty for all 3 to go into?
-
-// Another variable to randomly select but I need all list of all possible combinations
-
-// let pronounIndex = Math.floor(Math.random() * pronoun.length);
-// let adjIndex = Math.floor(Math.random() * adj.length);
-// let nounIndex = Math.floor(Math.random() * noun.length);
-
-//You'll need to use nested for loops in order to mix the different values together for loop string concat
-// for (let i = 0; i <) {} if
-
-// innerhtml to add the string together I'd try consol log all of this but nothing is showig
-// document.getElementById("domain").innerHTML =
-//   pronoun[pronounIndex] + adj[adjIndex] + noun[nounIndex] + ".com";
-
-//Show something on the console and of course it's not showing up in console
-// But I want to console log all 8
-// console.log("any string");
